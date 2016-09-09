bdb36f70511526b88979fcf9c6f59770a0c3469f
yvon-c <vonyfchang@gmail.com>
2016-09-09 10:53:19 +1000

    Update LysN15_C1_2014-15_18042016.R
    added comments explaining lysimeter surface area

--- LysN15_C1_2014-15_18042016.R
+++ LysN15_C1_2014-15_18042016.R
@@ -15,7 +15,7 @@ Lys$TotN.kg<-Lys$TN.mg.L*(Lys$Water.Volume..mL./1000)/1000/1000 # kg
 Ao<-0.3663 #(% N15 natural abundance)
 Lys$Ndff<-(Lys$X..AtomN15-Ao)/(35.16-Ao)*100
 Lys$N15N.kg<-Lys$TotN.kg*(Lys$X..AtomN15-Ao) # kg
-Lys$N15N.kgha<-Lys$N15N.kg/1.575254*10000
+Lys$N15N.kgha<-Lys$N15N.kg/1.575254*10000 # 1.575254 m2 = area of lysimeter trays
 N15applied.kgha<-180*(35.16/100) # 35.16% enrichment (kg/ha)
 Lys$PercentageFertN<-Lys$N15N.kgha/N15applied.kgha
 Lys$FertN.kgha<-Lys$PercentageFertN/100*(180+52)
